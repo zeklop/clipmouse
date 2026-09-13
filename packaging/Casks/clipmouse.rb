4d31025b358b097ee05e95a4029fcadaefea19b8
zeklop <zeklop@gmail.com>
2026-09-13 14:30:54 +0300

    cask: clipmouse v0.2.1 (sha256 от ClipMouse-0.2.1.dmg)

--- packaging/Casks/clipmouse.rb
+++ packaging/Casks/clipmouse.rb
@@ -1,6 +1,6 @@
 cask "clipmouse" do
-  version "0.2.0"
-  sha256 "2ba80668576cc7f843f99b25ef019e996aaffd214361ca23ff60fea32d422424"
+  version "0.2.1"
+  sha256 "2ef746860640d4f1c138a29982268d061ae440afe93506c0d1b6b1245196f183"
 
   url "https://github.com/zeklop/clipmouse/releases/download/v#{version}/ClipMouse-#{version}.dmg"
   name "ClipMouse"
